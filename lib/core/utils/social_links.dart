17e713c2038845a546c6350c75134cbe3cf58526
sanamrahimi <sanam.rahimi099@gmail.com>
2025-08-20 12:45:27 +0500

    fixed url

--- lib/core/utils/social_links.dart
+++ lib/core/utils/social_links.dart
@@ -2,5 +2,12 @@ abstract class SocialLinks {
   static const String github = "https://github.com/sanamrahimi";
   static const String email = "sanamrahimi019@gmail.com";
   static const String linkedin = "https://www.linkedin.com/in/sanamrahimi";
-  static const String cvLink = "https://drive.google.com/file/d/1CmCSwMXfzBE1r3avJkmaoIKKpcUzBwJN/view?usp=drive_link";
+  static const String cvLink = "https://drive.google.com/file/d/1nY35jjKFMMp5qpPOYKqP_3GkSwUUHlvh/view?usp=drive_link";
 }
+
+
+
+
+
+//git push origin main
+//git pull --rebase origin main
